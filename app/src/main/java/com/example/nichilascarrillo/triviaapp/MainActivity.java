3109b49dd12a03f9c2ee2872aa242eedaead60f5
Nicholas Carrillo <nicholasmcxbox@gmail.com>
2018-06-19 14:16:44 -0400

    added question creator fragment

--- app/src/main/java/com/example/nichilascarrillo/triviaapp/MainActivity.java
+++ app/src/main/java/com/example/nichilascarrillo/triviaapp/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.nichilascarrillo.triviaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+   private QuestionCreatorFragment questionCreatorFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
+    @OnClick(R.id.add_question_button)
+    protected void addQuestion() {
+
+        questionCreatorFragment = QuestionCreatorFragment.newInstance();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_holder, questionCreatorFragment).commit();
+
+    }
+
 }
