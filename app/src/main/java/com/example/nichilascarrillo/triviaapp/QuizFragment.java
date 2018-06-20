eee508088c939612905025960188a9dd910dff35
Nicholas Carrillo <nicholasmcxbox@gmail.com>
2018-06-20 11:01:30 -0400

    created quiz fragment class and layout

--- app/src/main/java/com/example/nichilascarrillo/triviaapp/QuizFragment.java
+++ app/src/main/java/com/example/nichilascarrillo/triviaapp/QuizFragment.java
@@ -9,9 +9,36 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class QuizFragment extends Fragment {
+
+
+    @BindView(R.id.correct_answer_button)
+    protected void buttonOneClicked() {
+
+    }
+
+    @BindView(R.id.first_wrong_answer_button)
+    protected void buttonTwoClicked() {
+
+    }
+
+    @BindView(R.id.second_wrong_answer_button)
+    protected void buttonTwoClicked() {
+
+    }
+
+    @BindView(R.id.third_wrong_answer_button)
+    protected void thirdWrongAnswerButton() {
+
+    }
+
+    @BindView(R.id.next_question_button)
+    protected void nextQuestionButton() {
+
+    }
 
 
     @Nullable
@@ -19,10 +46,8 @@ public class QuizFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
 
-
-
         View view = inflater.inflate(R.layout.fragment_quiz, container, false);
-        ButterKnife.bind( this, view);
+        ButterKnife.bind(this, view);
         return view;
     }
 
@@ -36,11 +61,6 @@ public class QuizFragment extends Fragment {
 
 
     }
-
-
-
-
-
 
 
 }
---
+++ app/src/main/java/com/example/nichilascarrillo/triviaapp/question.java
@@ -0,0 +1,32 @@
+package com.example.nichilascarrillo.triviaapp;
+
+public class question {
+
+
+    private String question;
+    private String correctAnswer;
+    private String firstWrongAnswer;
+    private String secondWrongAnswer;
+    private String thirdWrongAnswer;
+
+
+    public question(String question, String correctAnswer, String firstWrongAnswer, String secondWrongAnswer, String thirdWrongAnswer) {
+        this.question = question;
+        this.correctAnswer = correctAnswer;
+        this.firstWrongAnswer = firstWrongAnswer;
+        this.secondWrongAnswer = secondWrongAnswer;
+        this.thirdWrongAnswer = thirdWrongAnswer;
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
